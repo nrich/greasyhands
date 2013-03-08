e54d1a94ac806f63538d6b4718c4a4d950add476
Neil Richardson <nrich@ii.net>
2013-03-08 13:52:53 +0800

    Added a job to enque updates

---
+++ GreasyHands/Jobs/ScheduleGrabber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+using GreasyHands.Schedule;
+
+namespace GreasyHands.Jobs
+{
+    public class ScheduleGrabber
+    {
+        public ScheduleGrabber()
+        {
+        }
+
+        // ReSharper disable FunctionNeverReturns
+        public void Run()
+        {
+            Thread.CurrentThread.Name = "Schedule Grabber";
+
+            while (true)
+            {
+                ScheduleUpdater.AddReleaseURL(new Uri("http://www.previewsworld.com/shipping/newreleases.txt"));
+                Thread.Sleep(3600 * 1000);
+            }
+
+        }
+        // ReSharper restore FunctionNeverReturns
+    }
+}
